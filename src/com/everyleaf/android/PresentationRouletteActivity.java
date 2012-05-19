fde8589560f37c608b280150c50a69ebc23bb365
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-05-20 06:54:02 +0900

    initial commit

---
+++ src/com/everyleaf/android/PresentationRouletteActivity.java
@@ -0,0 +1,157 @@
+package com.everyleaf.android;
+
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class PresentationRouletteActivity extends Activity {
+	@SuppressWarnings("unused")
+	private final String TAG = this.getClass().getSimpleName();
+
+	private String[] TEXTS = { "Red", "Green", "Blue", "Purple", "Yellow" };
+	private ArrayList<String> texts = new ArrayList<String>();
+	private ArrayList<String> finished_texts = new ArrayList<String>();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.main);
+
+		setTypeface();
+	}
+
+	private void setTypeface() {
+		Typeface tf = Typeface
+				.createFromAsset(getAssets(), "JohnHancockCP.otf");
+		TextView text = (TextView) findViewById(R.id.result_text);
+		text.setTypeface(tf);
+		text = (TextView) findViewById(R.id.finished_texts);
+		text.setTypeface(tf);
+		Button button = (Button) findViewById(R.id.go);
+		button.setTypeface(tf);
+		button = (Button) findViewById(R.id.reset);
+		button.setTypeface(tf);
+	}
+
+	public void go(View view) {
+		if (texts.isEmpty()) {
+			initializeKujibiki();
+		}
+		rouletteResultText();
+	}
+
+	private void updateResultText() {
+		String result = texts.get(getRandomizeInt(texts.size()));
+		TextView text = (TextView) findViewById(R.id.result_text);
+		text.setText(result);
+		finished_texts.add(texts.remove(texts.indexOf(result)));
+	}
+
+	private void enableButtons(boolean enable) {
+		Button button = null;
+		button = (Button) findViewById(R.id.go);
+		button.setEnabled(enable);
+		button = (Button) findViewById(R.id.reset);
+		button.setEnabled(enable);
+	}
+
+	private void rouletteResultText() {
+		new AsyncTask<String, Integer, String>() {
+			@Override
+			protected void onPreExecute() {
+				enableButtons(false);
+			}
+
+			@Override
+			protected String doInBackground(String... params) {
+				int ROULETTE_TIME = 400;
+				int STOP_TIME = 40;
+				int COUNT = ROULETTE_TIME + STOP_TIME;
+				int sleep = 1;
+				for (int i = 0; i < COUNT; i++) {
+					publishProgress(i);
+					try {
+						Thread.sleep(sleep);
+					} catch (InterruptedException e) {
+					}
+					if (i > ROULETTE_TIME) {
+						sleep += i - ROULETTE_TIME;
+					}
+				}
+				return "";
+			}
+
+			@Override
+			protected void onProgressUpdate(Integer... values) {
+				TextView text = (TextView) findViewById(R.id.result_text);
+				text.setText(texts.get(values[0] % texts.size()));
+			}
+
+			@Override
+			protected void onPostExecute(String result) {
+				updateResultText();
+				updateFinishedTexts();
+				enableButtons(true);
+			}
+		}.execute(new String[] {});
+	}
+
+	private void initializeKujibiki() {
+		resetFields();
+		prepareTexts();
+	}
+
+	private void resetFields() {
+		TextView text = null;
+		text = (TextView) findViewById(R.id.result_text);
+		text.setText("");
+		text = (TextView) findViewById(R.id.finished_texts);
+		text.setText("");
+	}
+
+	private int getRandomizeInt(int size) {
+		if (size == 0) {
+			return 0;
+		}
+		return (int) (Math.random() * 10 % size);
+	}
+
+	private void prepareTexts() {
+		texts.clear();
+		for (String str : TEXTS) {
+			texts.add(str);
+		}
+		finished_texts.clear();
+	}
+
+	private void updateFinishedTexts() {
+		String strings = "";
+		int i = 1;
+		for (String str : finished_texts) {
+			strings = strings + i + ". " + str + "\n";
+			i++;
+		}
+		TextView text = (TextView) findViewById(R.id.finished_texts);
+		text.setText(strings);
+
+		// TODO: リファクタリング
+		if (texts.size() == 1) {
+			String result = texts.get(getRandomizeInt(texts.size()));
+			finished_texts.add(texts.remove(texts.indexOf(result)));
+			updateFinishedTexts();
+		}
+	}
+
+	public void reset(View view) {
+		prepareTexts();
+		resetFields();
+	}
+}
